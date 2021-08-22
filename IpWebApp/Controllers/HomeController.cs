825e1c65d67b0c8547052086adbcba11fe8521ed
Shir Fogel <shirfogel@cs.colman.ac.il>
2021-08-22 15:30:14 +0300

    tweeter upload

--- IpWebApp/Controllers/HomeController.cs
+++ IpWebApp/Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using TweetSharp;
+using System.Drawing;
 
 namespace IpWebApp.Controllers
 {
@@ -39,6 +42,45 @@ namespace IpWebApp.Controllers
         public ActionResult supportUs()
         {
             
+            return View();
+        }
+        [HttpPost]
+        public ActionResult supportUs(string message, HttpPostedFileBase image)
+        {
+            string key = "JagzH3v3wvj8nk1DS4NcwzFd3";
+            string secret = "9YLUOQgwRjLoXG6GQvSTnGC9QPAF8x1KVZka9xf2h1albz7NP2";
+            string token = "1106283607650381824-mcTFEj3K06QJjIjnNV9aN6rbJkF4TP";
+            string tokenSecret = "p8Z7NZOttul0NuAKvRVbx2vPOdRLttwQ0pDBt2Y7FZSoR";
+            string imagePath="";
+            //Enter the Image Path if you want to upload image .
+            if (image != null)
+                 imagePath = Path.GetFullPath(image.FileName);
+          
+            var service = new TweetSharp.TwitterService(key, secret);
+            service.AuthenticateWith(token, tokenSecret);
+
+            //this Condition  will check weather you want to upload a image & text or only text 
+            if (imagePath.Length > 0)
+            {
+                using (var stream = new FileStream(imagePath, FileMode.Open))
+                {
+                    var result = service.SendTweetWithMedia(new SendTweetWithMediaOptions
+                    {
+                        Status = message,
+                        Images = new Dictionary<string, Stream> { { "john", stream } }
+                    });
+                }
+            }
+            else // just message
+            {
+                var result = service.SendTweet(new SendTweetOptions
+                {
+                    Status = message
+                });
+
+            }
+
+           
             return View();
         }
         [Authorize]
