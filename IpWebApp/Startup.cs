9cb943c303318eab102d003536dbdfc0d4625d82
DESKTOP-NFKEKDB\guylevy <guylevy2307@gmail.com>
2021-08-27 13:11:23 +0300

    add role table

--- IpWebApp/Startup.cs
+++ IpWebApp/Startup.cs
@@ -4,6 +4,9 @@ using Hangfire;
 using Hangfire.SqlServer;
 using Hangfire.Storage;
 using System;
+using IpWebApp.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 [assembly: OwinStartupAttribute(typeof(IpWebApp.Startup))]
 namespace IpWebApp
@@ -33,6 +36,52 @@ namespace IpWebApp
             ////  RecurringJob.AddOrUpdate(() => temp.SendEmail(),Cron.Minutely());
             //RecurringJob.AddOrUpdate(() => temp.SendEmail(), Cron.Daily(9));
 
+            createRolesandUsers();
+        }
+        // In this method we will create default User roles and Admin user for login    
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            // creating first Admin Role and creating a default Admin User     
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin role   
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                   
+
+                var user = new ApplicationUser();
+                user.UserName = "admin";
+                user.Email = "admin@gmail.com";
+
+                string userPWD = "Adminadmin!";
+
+                var chkUser = userManager.Create(user, userPWD);
+
+                //Add default User to Role Admin    
+                if (chkUser.Succeeded)
+                {
+                    var result1 = userManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+
+            // creating Creating Manager role     
+            if (!roleManager.RoleExists("Client"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Client";
+                roleManager.Create(role);
+
+            }
 
         }
     }
