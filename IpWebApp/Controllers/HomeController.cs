b30947034cfa6a4e94329c512936c54d7d2e8c29
DESKTOP-NFKEKDB\guylevy <guylevy2307@gmail.com>
2021-09-01 19:49:22 +0300

    tweeter update

--- IpWebApp/Controllers/HomeController.cs
+++ IpWebApp/Controllers/HomeController.cs
@@ -45,17 +45,13 @@ namespace IpWebApp.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult supportUs(string message, HttpPostedFileBase image)
+        public ActionResult supportUs(string message)
         {
             string key = "JagzH3v3wvj8nk1DS4NcwzFd3";
             string secret = "9YLUOQgwRjLoXG6GQvSTnGC9QPAF8x1KVZka9xf2h1albz7NP2";
             string token = "1106283607650381824-mcTFEj3K06QJjIjnNV9aN6rbJkF4TP";
             string tokenSecret = "p8Z7NZOttul0NuAKvRVbx2vPOdRLttwQ0pDBt2Y7FZSoR";
             string imagePath="";
-            //Enter the Image Path if you want to upload image .
-            if (image != null)
-                 imagePath = Path.GetFullPath(image.FileName);
-          
             var service = new TweetSharp.TwitterService(key, secret);
             service.AuthenticateWith(token, tokenSecret);
 
@@ -75,8 +71,8 @@ namespace IpWebApp.Controllers
             {
                 var result = service.SendTweet(new SendTweetOptions
                 {
-                    Status = message
-                });
+                    Status = message + "   @" + User.Identity.Name
+            });
 
             }
 
